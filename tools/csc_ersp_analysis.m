132818b581217e00fbb7eefa06f7c9e24f10db9e
Mensen <research.mensen@gmail.com>
2017-11-09 23:08:32 +0100

    added own event related spectral perturbation function with output similar to EEGLABs newtimef

---
+++ tools/csc_ersp_analysis.m
@@ -0,0 +1,78 @@
+function [wavelet_series, freq_range, trial_wavelet] = csc_ersp_analysis(EEG)
+% function similar to newtimef with matlab's own continuous wavelet transform
+
+% remove bad trials
+if isfield (EEG, 'good_trials')
+    EEG.data = EEG.data(:, :, EEG.good_trials);
+    EEG.trials = sum(EEG.good_trials);
+end
+
+% define baseline period
+baseline_period = EEG.times >= -400 & EEG.times <= -100;
+normalisation_period = EEG.times >= -600 & EEG.times <= 600;
+
+% run single trial to get output sizes
+[~, freq_range, ~] = cwt(...
+    double(EEG.data(1, :, 1)), ...
+    'amor', EEG.srate);
+no_freqs = length(freq_range);     
+        
+% pre-allocate wavelet_series
+trial_wavelet = nan(no_freqs, EEG.pnts, EEG.trials);
+wavelet_series = nan(no_freqs, EEG.nbchan, EEG.pnts);
+
+% loop over all trials and channels and keep the mean
+swa_progress_indicator('initialise', 'number of trials complete');
+for nCh = 1 : EEG.nbchan
+    swa_progress_indicator('update', nCh, EEG.nbchan);
+
+    for nT = 1 : EEG.trials
+        trial_wavelet(:, :, nT) = cwt(...
+            double(EEG.data(nCh, :, nT)), ...
+            'amor', EEG.srate);
+    end
+    
+    % calculate wavelet power
+    wavelet_power = trial_wavelet .* conj(trial_wavelet);
+    
+    % normalise by full time series mean (like newtimef
+    % NOTE: each trial has its own mean
+    % TODO: explore whether trial or whole series normalisation is better
+    mean_wavelet_power = mean(wavelet_power(:, normalisation_period, :), 2);
+    norm_wavelet_power = wavelet_power ...
+        ./repmat(mean_wavelet_power, [1, size(wavelet_power, 2), 1] );
+    
+    % calculate the trial by trial baseline
+    trial_baseline = mean(wavelet_power(:, baseline_period, :), 2);
+    
+    % calculate overall baseline mean
+    % NOTE: baseline taken from averaged time series
+    temp_mean = mean(norm_wavelet_power, 3);
+    overall_baseline = mean(temp_mean(:, baseline_period), 2);
+    
+    % subtract the trial baseline (each trial from its own)
+%     corrected_series = norm_wavelet_power - trial_baseline(:, ones(EEG.pnts, 1), :);
+    
+    % or divide by baseline (almost like newtimef)
+%     corrected_series = norm_wavelet_power ...
+%         ./repmat(trial_baseline, [1, size(norm_wavelet_power, 2), 1] );
+    
+    % or divide by overall baseline (like newtimef)
+    corrected_series = bsxfun(@rdivide, norm_wavelet_power, overall_baseline);
+      
+    % get the absolute mean
+    wavelet_series(:, nCh, :) = 10 * log10(sqrt(mean(abs(corrected_series) .^ 2, 3)));
+    
+end
+    
+if flag_plot
+   
+    selected_channel = 1;
+    selected_freqs = freq_range < 50;
+    
+    figure('color', 'w');
+    contourf(EEG.times(normalisation_period), freq_range(selected_freqs), ...
+        squeeze(wavelet_series(selected_freqs, selected_channel, normalisation_period)), ...
+        'linestyle', 'none');
+    
+end
